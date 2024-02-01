f909068611d234627ce1b8bb533f497bb8447fc0
Sergei Maertens <sergei@maykinmedia.nl>
2024-02-01 17:45:53 +0100

    :sparkles: Closes #10 -- add python paths of django-webtest auth backends

--- maykin_2fa/test/__init__.py
+++ maykin_2fa/test/__init__.py
@@ -1,6 +1,13 @@
 from django.conf import settings
 from django.test import override_settings
 
+# Webtest backends, taken from https://github.com/django-webtest/django-webtest/blob/
+# 6370c1afe034da416b03b2f88b7c71b9a49122c6/django_webtest/backends.py
+DJANGO_WEBTEST_BACKENDS = (
+    "django_webtest.backends.WebtestUserBackend",
+    "django_webtest.backends.WebtestUserWithoutPermissionsBackend",
+)
+
 
 def disable_admin_mfa():
     """
@@ -9,6 +16,6 @@ def disable_admin_mfa():
     Based on :func:`django.test.override_settings`, so you can use it as a decorator
     or context manager.
     """
-    return override_settings(
-        MAYKIN_2FA_ALLOW_MFA_BYPASS_BACKENDS=settings.AUTHENTICATION_BACKENDS,
-    )
+    django_backends = settings.AUTHENTICATION_BACKENDS
+    all_backends = django_backends + list(DJANGO_WEBTEST_BACKENDS)
+    return override_settings(MAYKIN_2FA_ALLOW_MFA_BYPASS_BACKENDS=all_backends)
